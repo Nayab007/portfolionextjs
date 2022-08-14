79283b0e2b17e4525016c6cf6ccccf6b140856ef
Nayab Lone <55736152+Nayab007@users.noreply.github.com>
2022-08-14 13:07:29 +0200

    add

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -19,7 +19,7 @@ const Header = () =>  (
       <li>
       <Reveal left big>
         <Link href="#tech">
-          <NavLink>Tech</NavLink>
+          <NavLink>Tech S</NavLink>
         </Link>
         </Reveal>
       </li>
